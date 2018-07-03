9fe3ceb999a9b6ab47cc41d12dd752565163af0a
venugopal990 <venugopal.dummu@gmail.com>
2018-07-03 09:32:35 +0530

    Update Login_Register.php

--- Login_Register.php
+++ Login_Register.php
@@ -4,7 +4,7 @@
 <head>
 <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
 <link href="design.css" type="text/css" rel="stylesheet"/>
-<?php include 'functions.php';?>
+<?php include 'registration.php';?>
 </head>
 <body>
 
